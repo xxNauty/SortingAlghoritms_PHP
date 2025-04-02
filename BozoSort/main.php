299ef8d62c5bf22622d9f8d269d7013eaa535271
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-04-02 23:15:56 +0200

    portion of stupid sorting algorithms, some fixes of older implementations

--- BozoSort/main.php
+++ BozoSort/main.php
@@ -1,1 +1,41 @@
 <?php
+
+function bogosort(array $data): array
+{
+    while(true){
+        if(!isSorted($data)){
+            shuffle($data);
+        }
+        else{
+            return $data;
+        }
+    }
+}
+
+function isSorted(array $data): bool
+{
+    for ($i = 0; $i < count($data) - 1; $i++) {
+        if($data[$i] > $data[$i+1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function mix(array &$data): void
+{
+    for($i = 0; $i < count($data); $i++ ){
+        swap(
+            $data[rand(0, count($data))],
+            $data[rand(0, count($data))]
+        );
+    }
+}
+
+function swap(&$a, &$b): void
+{
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
