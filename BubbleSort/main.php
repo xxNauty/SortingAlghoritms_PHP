87608c61570491e4842f371fa6cbe2236bdbe2bc
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-04-04 22:58:53 +0200

    code style fixes

--- BubbleSort/main.php
+++ BubbleSort/main.php
@@ -4,9 +4,9 @@ require_once "../Common/swap.php";
 
 function bubbleSort(array $data): array
 {
-    for($i = 0; $i < count($data); $i++){
-        for($j = $i + 1; $j < count($data); $j++){
-            if($data[$i] > $data[$j]){
+    for ($i = 0; $i < count($data); $i++) {
+        for ($j = $i + 1; $j < count($data); $j++) {
+            if ($data[$i] > $data[$j]) {
                 swap($data[$i], $data[$j]);
             }
         }
