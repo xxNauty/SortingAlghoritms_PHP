2a774ae1b0d06d082df8819e7075241d82eddafd
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-04-04 22:54:10 +0200

    codesniffer fix

--- Common/is_sorted.php
+++ Common/is_sorted.php
@@ -3,7 +3,7 @@
 function isSorted(array $data): bool
 {
     for ($i = 0; $i < count($data) - 1; $i++) {
-        if($data[$i] > $data[$i+1]) {
+        if ($data[$i] > $data[$i + 1]) {
             return false;
         }
     }
